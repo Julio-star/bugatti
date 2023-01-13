d36cfb35b7036715492e000c8bd1c7e45c2ac9e2
Julio-star <58565784+Julio-star@users.noreply.github.com>
2023-01-13 12:50:05 +0100

    Again

--- src/core/components/side-bar/side-bar.js
+++ src/core/components/side-bar/side-bar.js
@@ -4,12 +4,12 @@ import './side-bar.css';
 
 const SideBar = (params) => {
     let [number, setNumber] = useState(0);
-
+    console.log(number);
     return (
         <div className="sideBar">
             <h2>{params.title}</h2><br />
             <ul>
-                <li><Button onClick={() => setNumber(params.firstParams)}>{params.firstParams}</Button></li>
+                <li><Button onClick={() => setNumber(1)}>{params.firstParams}</Button></li>
                 <li><Button onClick={() => setNumber(2)}>{params.secondParams}</Button></li>
                 <li><Button onClick={() => setNumber(3)}>{params.thirdParams}</Button></li>
                 <li><Button onClick={() => setNumber(4)}>{params.fourthParams}</Button></li>
